dd5d403d4b4264081386917e5c5e9b14dbd1eb60
Amelia Trevino <trevinoamelia25@gmail.com>
2024-09-28 12:20:19 -0500

    Django's Token Authentication

--- server/secure_storage/secure_storage/settings.py
+++ server/secure_storage/secure_storage/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'files'
+    'files',
+    'rest_framework',
+    'rest_framework.authtoken'
 ]
 
 MIDDLEWARE = [
